a37860c3bd88c1d73ff3024b39685f099d6b2a83
Samuel Monsalve Gomez <monsalvegomezsamuel2@gmail.com>
2025-05-06 18:54:06 -0500

    Correccion de errores

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import OldPasswordPage from './pages/OldPasswordPage/OldPasswordPage.jsx';
 import LoginPage from './pages/LoginPage/LoginPage.jsx' /* Importamos el componente LoginPage */
 import UpdatePasswordPage from './pages/UpdatePasswordPage/UpdatePasswordPage'; /* Importamos el componente UpdatePasswordPage */
-import VerifyCodePage from './pages/VerifyCodePage/VerifyCodePage.jsx';
+import VerifyCodePage from './pages/VerifyCodePage/VerifyCodePage.jsx'
 
 function App() {
   return (
@@ -14,7 +14,7 @@ function App() {
         <Route path="/" element={<LoginPage/>} /> {/* Definimos la ruta para el componente LoginPage */}
         <Route path="/old" element={<OldPasswordPage/>} /> {/* Definimos la ruta para el componente OldPassword */}
         <Route path="/update" element={<UpdatePasswordPage/>} /> {/* Definimos la ruta para el componente UpdatePassword */}
-        <Route path="/verify" element={<VerifyCodePage/>} /> {/* Definimos la ruta para el componente VerifyCodePage */}
+        <Route path='/verify' element={<VerifyCodePage/>} />
       </Routes>
     </BrowserRouter>
     </>
