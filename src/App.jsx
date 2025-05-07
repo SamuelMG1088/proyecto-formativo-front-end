4f90fab00ca452a130035acec56375633e91d209
Samuel Monsalve Gomez <monsalvegomezsamuel2@gmail.com>
2025-05-07 14:12:48 -0500

    Primer commit en la branch samuel

--- src/App.jsx
+++ src/App.jsx
@@ -4,23 +4,11 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import OldPasswordPage from './pages/OldPasswordPage/OldPasswordPage.jsx';
 import LoginPage from './pages/LoginPage/LoginPage.jsx' /* Importamos el componente LoginPage */
 import UpdatePasswordPage from './pages/UpdatePasswordPage/UpdatePasswordPage'; /* Importamos el componente UpdatePasswordPage */
-<<<<<<< HEAD
-import VerifyCodePage from './pages/VerifyCodePage/VerifyCodePage.jsx';
-import Navigation from './layout/SideBar/SideBar.jsx';
-import { NavigationProvider } from './layout/SideBar/SideBar.jsx';
-=======
 import VerifyCodePage from './pages/VerifyCodePage/VerifyCodePage.jsx'
->>>>>>> a37860c3bd88c1d73ff3024b39685f099d6b2a83
 
 function App() {
   return (
     <>
-<<<<<<< HEAD
-      <NavigationProvider>
-        <Navigation/>
-      </NavigationProvider>
-    
-=======
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<LoginPage/>} /> {/* Definimos la ruta para el componente LoginPage */}
@@ -29,7 +17,6 @@ function App() {
         <Route path='/verify' element={<VerifyCodePage/>} />
       </Routes>
     </BrowserRouter>
->>>>>>> a37860c3bd88c1d73ff3024b39685f099d6b2a83
     </>
   )
 }
