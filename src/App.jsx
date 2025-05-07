26a3051025db531d2e076a27347466cd0edda809
Samuel Monsalve Gomez <monsalvegomezsamuel2@gmail.com>
2025-05-07 16:00:42 -0500

    Home

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,23 @@
 import './App.css'
-import { useState } from 'react'
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import LoginPage from './pages/LoginPage/LoginPage.jsx';
 import OldPasswordPage from './pages/OldPasswordPage/OldPasswordPage.jsx';
-import LoginPage from './pages/LoginPage/LoginPage.jsx' /* Importamos el componente LoginPage */
-import UpdatePasswordPage from './pages/UpdatePasswordPage/UpdatePasswordPage'; /* Importamos el componente UpdatePasswordPage */
-import VerifyCodePage from './pages/VerifyCodePage/VerifyCodePage.jsx'
+import UpdatePasswordPage from './pages/UpdatePasswordPage/UpdatePasswordPage.jsx';
+import VerifyCodePage from './pages/VerifyCodePage/VerifyCodePage.jsx';
+import Home from './pages/Home/Home.jsx';
 
 function App() {
   return (
     <>
     <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<LoginPage/>} /> {/* Definimos la ruta para el componente LoginPage */}
-        <Route path="/old" element={<OldPasswordPage/>} /> {/* Definimos la ruta para el componente OldPassword */}
-        <Route path="/update" element={<UpdatePasswordPage/>} /> {/* Definimos la ruta para el componente UpdatePassword */}
-        <Route path='/verify' element={<VerifyCodePage/>} />
-      </Routes>
+        <Routes>
+          <Route path="/" element={<LoginPage/>} />
+          <Route path="/oldpassword" element={<OldPasswordPage/>} />
+          <Route path="/updatepassword" element={<UpdatePasswordPage/>} />
+          <Route path="/verifycode" element={<VerifyCodePage/>} />
+          <Route path="home" element={<Home/>} />
+        </Routes>
+      
     </BrowserRouter>
     </>
   )
