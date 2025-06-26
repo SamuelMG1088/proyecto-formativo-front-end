1ceb3b5d6513acb95220a2f5419850b0bc3d0852
Samuel Monsalve Gomez <monsalvegomezsamuel2@gmail.com>
2025-06-26 15:05:36 -0500

    modificaciones

--- src/components/ExportPdfExcel/ExportPdfExcel.jsx
+++ src/components/ExportPdfExcel/ExportPdfExcel.jsx
@@ -3,16 +3,19 @@ import './css/exportPdfExcel.css' /* Importamos la libreria de react - Import re
 import '../../styles/variables.css'  /*Importamos el archivo de variables - */ 
 import { ImFilePdf } from "react-icons/im";
 import { GrDocumentWindows } from "react-icons/gr";
+import { RiFileExcel2Fill } from "react-icons/ri";
+import { MdPictureAsPdf } from "react-icons/md";
+
 
 export default function ExportPdfExcel () { /* Definimos el Componente - Define the component*/
   return ( /* Renderizamos el contenido del componente - Render the component of the content*/
     <div id='Buttons-exports'>
         <div className="exports">
             <div className="box-exports pdf">
-                <ImFilePdf className="logo-pdf"/>
+                <MdPictureAsPdf className="logo-pdf"/>
             </div>
             <div className="box-exports">
-                <GrDocumentWindows className="logo-exel"/>
+                <RiFileExcel2Fill className="logo-exel"/>
             </div>
         </div>
     </div>
